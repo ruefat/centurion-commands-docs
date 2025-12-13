7369985362f25aebcc2898ec746f207908a7c498
Lukas <r.yusifli@yahoo.com>
2025-12-13 12:10:04 +0100

    Updated docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'dev-lukas0',
   projectName: 'centurion-commands-docs',
+  trailingSlash: true,
   //deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
